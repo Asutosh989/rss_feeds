0b944eb984352254e76fdb62d5cb9483592cef9a
Asutosh989 <asutoshpanda989@gmail.com>
2017-08-23 22:27:20 +0530

    Add feed parser

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var feedparser = require('ortoo-feedparser');
+
 var router = express.Router();
+
 
 
 /* GET users listing. */
@@ -10,9 +13,15 @@ var router = express.Router();
 router.post('/login', function(req, res){
   var username = req.body.username;
   var password = req.body.password;
+  var url = "http://theindiansociety.org/feed";
+
+  feedparser.parseUrl(url).on('article', (article) => {
+  	var title = article.title;
+  	var desc = article.description;
+  });
 
   if (username == "Admin" && password == "1234"){
-    res.render('dashboard');
+    res.render('dashboard',{title:title});
   }
   else {
     req.flash('error_msg', {"error":"Try Again"});
